e04a51cdf3d17d032610e55de5ce39dcc05f952c
Emmanuel Osorio Bustacara <osorioemmanuel79@gmail.com>
2026-04-07 17:05:41 -0500

    mainv1

---
+++ main.py
@@ -0,0 +1,48 @@
+from grades_manager import *
+
+def main():
+    print("Welcome to the Student Grades Manager!\n")
+
+    my_grades = {}
+
+    while True:
+        print("Select an option:")
+        print("1. Add a student")
+        print("2. Print student grade averages")
+        print("3. Exit")
+
+        choice = input().strip()
+
+        if choice == "1":
+            my_grades = add_student(my_grades)
+
+        elif choice == "2":
+            print("Select an option:")
+            print("a. Display all students")
+            print("b. Display selected students")
+
+            sub_choice = input().strip().lower()
+
+            if sub_choice == "a":
+                avg_by_student(my_grades)
+
+            elif sub_choice == "b":
+                names = input("Enter student names (comma-separated):\n")
+                keys = [name.strip() for name in names.split(",")]
+                avg_by_student(my_grades, keys)
+
+            else:
+                print("Invalid option selected!")
+
+        elif choice == "3":
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid option selected!")
+
+        print()  # Espacio para que se vea ordenado
+
+
+# Ejecutar programa
+main()
