a29cee832cc53d4e15c35cc988889f9f8ab7dda0
Emmanuel Osorio Bustacara <osorioemmanuel79@gmail.com>
2026-04-07 17:33:00 -0500

    ovv2

--- overall.py
+++ overall.py
@@ -1,11 +1,3 @@
-# Diccionario principal
-students = {
-    "s1": {"hw1": 80, "hw2": 90, "hw3": 100},
-    "s2": {"hw1": 70, "hw2": 75, "hw3": 85},
-    "s3": {"hw1": 95, "hw2": 85, "hw3": 90}
-}
-
-
 def student_averages(data):
     result = {}
 
@@ -19,17 +11,16 @@ def student_averages(data):
 def assignment_averages(data):
     result = {}
 
-    # Obtener todas las tareas desde el primer estudiante
-    assignments = list(next(iter(data.values())).keys())
+    # Obtener todas las tareas
+    assignments = next(iter(data.values())).keys()
 
     for assignment in assignments:
         total = 0
-        count = 0
 
         for grades in data.values():
             total += grades[assignment]
-            count += 1
 
-        result[assignment] = round(total / count)
+        avg = total / len(data)
+        result[assignment] = round(avg)
 
     return result
