05b277d78367f396f549d7da66b96e190b1f787d
Emmanuel Osorio Bustacara <osorioemmanuel79@gmail.com>
2026-04-07 16:57:45 -0500

    grademnv4

--- grades_manager.py
+++ grades_manager.py
@@ -1,27 +1,18 @@
-def add_student(student_grades=None):
-    if student_grades is None:
-        student_grades = {}
+def get_students(student_grades, keys):
+    result = {}
 
+    for name in keys:
+        name_title = name.title()
+        found = False
 
-    name = input("Enter student name:\n").title()
+        # Buscar sin importar mayúsculas/minúsculas
+        for student in student_grades:
+            if student.lower() == name.lower():
+                result[student] = student_grades[student]
+                found = True
+                break
 
-    subjects = {}
+        if not found:
+            print(f"{name_title} not found!")
 
-    while True:
-        entry = input("Enter subject and grade (or 'exit' to finish):\n")
-
-        if entry.lower() == "exit":
-            break
-
-        subject, grade = entry.split(",")
-
-        subject = subject.strip().title()
-        grade = float(grade.strip())
-
-        subjects[subject] = grade
-
-    student_grades[name] = subjects
-
-    print(f"Student {name} successfully added to the grades management system.")
-
-    return student_grades
+    return result
