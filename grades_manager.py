497ec108eca7eaedd0bdaa617f2e01d15a958a80
Emmanuel Osorio Bustacara <osorioemmanuel79@gmail.com>
2026-04-07 16:53:59 -0500

    grademanv2

--- grades_manager.py
+++ grades_manager.py
@@ -1,4 +1,27 @@
-def initialize_dict(student_name, subject_grades):
-    return {
-        student_name: subject_grades
-    }
+def add_student(student_grades=None):
+    if student_grades is None:
+        student_grades = {}
+
+    
+    name = input("Enter student name:\n").title()
+
+    subjects = {}
+
+    while True:
+        entry = input("Enter subject and grade (or 'exit' to finish):\n")
+
+        if entry.lower() == "exit":
+            break
+
+        subject, grade = entry.split(",")
+
+        subject = subject.strip().title()
+        grade = float(grade.strip())
+
+        subjects[subject] = grade
+
+    student_grades[name] = subjects
+
+    print(f"Student {name} successfully added to the grades management system.")
+
+    return student_grades
